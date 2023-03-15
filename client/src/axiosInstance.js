e115123dc5d28cc9323dcb063b9455f8c6cf661f
Mathius <info@yogimathius.dev>
2023-03-15 00:47:31 -0700

    fix: remove base url

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,11 +1,6 @@
 import axios from 'axios'
 
-const axiosInstance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? process.env.REACT_APP_API_BASE_URL
-      : 'http://localhost:8080/'
-})
+const axiosInstance = axios.create()
 
 axiosInstance.interceptors.request.use(
   (config) => {
